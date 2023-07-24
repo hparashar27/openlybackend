0875c8268401a0b43f12aececd5cfc57ff8313e7
Harsh <hparashar617@gmail.com>
2023-07-24 12:23:59 +0530

    final commit

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ mongoose.connect( process.env.DATABASE_URL,{
 }).then(con=>{
   console.log("DB Connection is established");
 });
-
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 // app.use("/api/opportunities", opportunitiesRoutes);
+
 app.use("/api", require("./routes/Members/index"));
 app.use("/api", require("./routes/Opportunities/index"));
 
@@ -17,7 +19,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello, World!' });
 });
 
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 9999
 if(port){
   app.listen(port, () => {
     console.log(`App running on port ${port} 🚀🚀🚀🚀`);
